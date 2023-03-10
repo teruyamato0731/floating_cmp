cd7bf003eb595fb14b1b92bd6ceb44cb1e4aa10e
Yoshikawa Teru <77840326+teruyamato0731@users.noreply.github.com>
2023-03-10 10:46:34 +0900

    Create main.cpp

---
+++ examples/main.cpp
@@ -0,0 +1,12 @@
+#include <floating_cmp.h>
+#include <iostream>
+
+int main() {
+    std::cout << floating_cmp(1.0, 0.0) << std::endl;
+    std::cout << floating_cmp(1.0, 1.0) << std::endl;
+    std::cout << floating_cmp(1.0, 2.0) << std::endl;
+  
+    std::cout << floating_cmp(1.0f, 0.0) << std::endl;
+    std::cout << floating_cmp(1.0, 1.0f) << std::endl;
+    std::cout << floating_cmp(1, 2) << std::endl;
+}
